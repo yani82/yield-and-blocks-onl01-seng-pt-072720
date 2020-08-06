40093b7d7df4f3c2cdbabd63331a58a2903af94b
Yani Asari <yani.iman1@gmail.com>
2020-08-06 18:29:48 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -7,8 +7,7 @@ def hello_t(array)
 end
 
 # call your method here!
-hello_t(["Tim", "Tom", "Jim"]) do |name|
+hello_t(["Tim", "Tom", "Jim"])|name|
   if name.start_with?("T")
     puts "Hi, #{name}"
-  end
 end
